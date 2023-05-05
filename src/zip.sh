7cba765671107f318b935891461afadd1aa3c5f3
A. Feder Cooper <afc78@g2-login-02.coecis.cornell.edu>
2023-05-05 00:18:31 -0400

    historical notebooks; remove data from being tracked

--- src/zip.sh
+++ src/zip.sh
@@ -1,4 +1,4 @@
-# run from within the wkspace directory
+# run from within the hmda repo root directory
 
 # don't include VI; only have data for 2017
 for state in AK  AZ  CT  FL  IA  IN  LA  ME  MO  NC  NH  NV  OK  PR  SD  UT  VT  WV AL  CA  DC  GA  ID  KS  MA  MI  MS  ND  NJ  NY  OR  RI  TN  VA  WA  WY AR  CO  DE  HI  IL  KY  MD  MN  MT  NE  NM  OH  PA  SC  TX WI
@@ -8,12 +8,10 @@ do
 	for year in 2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 2017
 	do
    		echo -e "\t$year"
-   		# join files for state and year
-   		awk '(NR ==1) || (FNR > 1)' $state/$year/split_* > $state/$year/$year-$state-all.csv
-   		zip $state/$year/$year-$state-all.zip $state/$year/$year-$state-all.csv
-
-   		# move to data dir in repo
-   		mv $state/$year/$year-$state-all.zip ../hmda/data/$state/$year
+   		# zip year/state
+   		zip data/$year/$state/$year-$state-features.zip data/$year/$state/$year-$state-features.csv
+		zip data/$year/$state/$year-$state-protected.zip data/$year/$state/$year-$state-protected.csv
+		zip data/$year/$state/$year-$state-target.zip data/$year/$state/$year-$state-target.csv
 	done
 	echo -e "\n"
 done
